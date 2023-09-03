70e6128c9eb5d364fad027aa9d4430782e44a007
Kumail Raza <kumailraza260@gmail.com>
2023-09-03 12:11:09 +0500

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './VotingProvider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,42 +17,65 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Voting App',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.pink,
       ),
-      home:  HomeScreen(),
+      home: ChangeNotifierProvider.value(value: VotingProvider(), child: HomeScreen(),), 
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+   HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController valueController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    
+    // ignore: avoid_print
+    print('Build');
+    
+        final dataProvider = Provider.of<VotingProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Voting App'),
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: valueController,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: valueController,
+                
+              ),
+              ElevatedButton(onPressed: (){
+                if(valueController.text.isNotEmpty){
+                  dataProvider.addItem(valueController.text);
+                  valueController.clear();
+                }
               
-            ),
-            ElevatedButton(onPressed: (){
-
-            }, child: const Text('Add Item'),),
-          ],
+              }, child: const Text('Add Item'),),
+              Consumer<VotingProvider>(builder: (_, data, __) => Expanded(
+                child: ListView.builder(
+                  itemCount: dataProvider.getData.length, 
+                  itemBuilder: (_, index) =>  Card(
+                    color: Colors.pink[100],
+                    
+                    child: ListTile(
+                      onTap: (){
+                        data.incVotes(index);
+                      },
+        
+                      title: Text('${data.getData[index]['title']}', style: TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold),), 
+                      trailing: CircleAvatar(
+                        child: Text('${data.getData[index]['votes']}'),
+                        ),
+                        ),
+                  ))),),
+            ],
+          ),
         ),
       ),
     );
